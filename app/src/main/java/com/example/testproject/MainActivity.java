9f27428224ef2785d62e9d9f4f60882f6e2bebf6
GEmanuil <gemanuil7@gmail.com>
2023-02-17 23:55:00 +0200

    OK THE PROBLEM IS BECAUSE IT IS NOT FULL SCREEN
    ----------------ONTOUCH SOLVED!!!-------------

--- app/src/main/java/com/example/testproject/MainActivity.java
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.testproject;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,8 +11,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mainactivity);
 
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.mainactivity);
         CuV = findViewById(R.id.middle);
 
     }
